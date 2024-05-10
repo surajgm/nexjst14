a6780d1c5d5fd4d87a68a9ab54334cc58f20f25f
Suraj gm <suraajgm@gmail.com>
2024-05-10 13:15:46 +0545

    Updated with navbar links and responsiveness

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,9 @@
+"use client";
+
 import Link from "next/link";
+import NavLink from "./navLink/NavLink";
+import styles from "./links.module.css";
+import { useState } from "react";
 
 const links = [
   {
@@ -19,14 +24,45 @@ const links = [
   },
 ];
 
+// Temporary
+const session = true;
+const isAdmin = true;
+
 const Links = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-      {links.map((link) => (
-        <Link key={link.title} href={link.path}>
-          {link.title}
-        </Link>
-      ))}
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink key={link.title} item={link} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink
+            item={{
+              title: "Login",
+              path: "/login",
+            }}
+          />
+        )}
+      </div>
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menu
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
